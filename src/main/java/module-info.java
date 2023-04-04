3d6d2786a375e1a1a2faaf040461dc5e5c1d89c1
guotongning <guotongning@58.com>
2023-04-04 21:53:50 +0800

    [initializer] 初始化学习JavaFX的项目

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,11 +1,10 @@
 module com.ning.fx.study.javafx_study {
     requires javafx.controls;
     requires javafx.fxml;
-    requires org.apache.commons.io;
-
 
     opens com.ning.fx.study.javafx_study to javafx.fxml;
     exports com.ning.fx.study.javafx_study;
-    exports com.ning.fx.study.javafx_study.hello;
+
     opens com.ning.fx.study.javafx_study.hello to javafx.fxml;
+    exports com.ning.fx.study.javafx_study.hello;
 }
